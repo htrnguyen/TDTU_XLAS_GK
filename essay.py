a9529c51c42d22d59e164080ef6bc0595bd3eb5a
nguyenn-04 <hatrongnguyen0423@gmail.com>
2024-04-02 23:58:42 +0700

    ver_2

--- essay.py
+++ essay.py
@@ -1,86 +1,153 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
-image_input_1 = cv2.imread("./input1.jpg")
-image_input_2 = cv2.imread("./input2.png")
+image_1 = cv2.imread("./input1.jpg")
 
-# --------------- 1a ---------------
-# Convert background to black
-image_a = image_input_1.copy()
-color_bg = np.array([127, 179, 216])
-diff = cv2.absdiff(image_a, color_bg)
-mask = cv2.inRange(diff, np.array([0, 0, 0]), np.array([70, 80, 100]))
-black_bg = np.zeros_like(image_a)
-image_a[mask == 255] = [0, 0, 0]
+
+# ------------------- 1 -------------------
+def get_common_color_RGB(image):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    # Calculate the histogram
+    hist = cv2.calcHist([hsv], [0, 1, 2], None, [8, 8, 8], [0, 180, 0, 256, 0, 256])
+    # Find the color with the most pixels
+    most_common_color_index = np.unravel_index(np.argmax(hist), hist.shape)
+    # Convert the index to actual color value
+    most_common_color = [(index / 8) * 255 for index in most_common_color_index]
+    # Convert the color from HSV to BGR
+    most_common_color_bgr = cv2.cvtColor(
+        np.uint8([[most_common_color]]), cv2.COLOR_HSV2BGR
+    )[0][0]
+    # Convert the color from BGR to RGB
+    most_common_color_rgb = most_common_color_bgr[::-1]
+
+    return most_common_color_rgb
+
+
+# ------------------- 1a -------------------
+image_1a = image_1.copy()
+# Convert background color to black
+background_color = get_common_color_RGB(image_1a)
+diff = cv2.absdiff(image_1a, background_color)
+mask = cv2.inRange(diff, np.array([0, 0, 0]), np.array([130, 85, 110]))
+black_bg = np.zeros_like(image_1a)
+image_1a[mask == 255] = [0, 0, 0]
 
 # List lower and upper
 color_dict_HSV = {
     "yellow": [[35, 255, 255], [25, 50, 70]],
-    "orange": [[24, 255, 255], [10, 50, 70]],
+    "orange": [[12, 255, 255], [10, 100, 100]],
     "red": [[180, 255, 255], [159, 50, 70]],
     "blue": [[128, 255, 255], [90, 50, 70]],
     "green": [[89, 255, 255], [36, 50, 70]],
     "purple": [[158, 255, 255], [129, 50, 70]],
 }
 # Convert to HSV
-image_hsv = cv2.cvtColor(image_a, cv2.COLOR_BGR2HSV)
+image_hsv = cv2.cvtColor(image_1a, cv2.COLOR_BGR2HSV)
 
 # Process each color
-image_outputs = []
 for color, (upper, lower) in color_dict_HSV.items():
     mask = cv2.inRange(image_hsv, np.array(lower), np.array(upper))
-    image_output = cv2.bitwise_and(image_a, image_a, mask=mask)
-    # Save image
+    image_output = cv2.bitwise_and(image_1a, image_1a, mask=mask)
+
+    # Save the image
     cv2.imwrite(f"./output_1a_{color}.jpg", image_output)
 
-# --------------- 1b ---------------
-image_gray_b = cv2.cvtColor(image_a, cv2.COLOR_BGR2GRAY)
-image_binary = cv2.threshold(image_gray_b, 0, 255, cv2.THRESH_BINARY_INV)[1]
-kernel = np.ones((4, 6), np.uint8)
-image_output_b = cv2.morphologyEx(image_binary, cv2.MORPH_OPEN, kernel)
-# Save image
-cv2.imwrite(f"./output_1b.jpg", image_output_b)
+# ------------------- 1b -------------------
+image_gray_1b = cv2.cvtColor(image_1a, cv2.COLOR_BGR2GRAY)
+thresh_binary = cv2.threshold(image_gray_1b, 0, 255, cv2.THRESH_BINARY_INV)[1]
+kernel = np.ones((6, 6), np.uint8)
+image_1b = cv2.morphologyEx(thresh_binary, cv2.MORPH_OPEN, kernel)
+cv2.imwrite("./output_1b.jpg", image_1b)
 
-# --------------- 2 ---------------
-image_2 = cv2.imread("./input2.png")
-image_gray_2 = cv2.cvtColor(image_2, cv2.COLOR_BGR2GRAY)
 
-# Get image ROI (region of interest)
-h, w = image_gray_2.shape
-roi_start = int(h * 0.35)
-roi = image_gray_2[roi_start:h, int(w - 0.6 * w) :]  # 35% of height and 60% of width
+# ------------------- 2 -------------------
+def find_ROI(image):
+    _, thresholded_image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV)
 
-# Process remove noise
-thresh_roi = cv2.threshold(roi, 30, 255, cv2.THRESH_BINARY_INV)[1]
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
-opening_roi = cv2.morphologyEx(thresh_roi, cv2.MORPH_OPEN, kernel, iterations=1)
-contours, _ = cv2.findContours(opening_roi, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-min_area = 50
-contours_to_remove = [cnt for cnt in contours if cv2.contourArea(cnt) < min_area]
-for cnt in contours_to_remove:
-    cv2.drawContours(opening_roi, [cnt], 0, 0, thickness=cv2.FILLED)
-opening_roi = cv2.dilate(opening_roi, kernel, iterations=1)
+    contours, _ = cv2.findContours(
+        thresholded_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
 
-# Process image gray
-thresh_img = cv2.adaptiveThreshold(
-    image_gray_2, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 51, 2
-)
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (60, 1))
-opening_img = cv2.morphologyEx(thresh_img, cv2.MORPH_OPEN, kernel)
-image_gray = cv2.subtract(thresh_img, opening_img)
+    def contour_area(contour):
+        _, _, w, h = cv2.boundingRect(contour)
+        return w * h
 
-# Combine image gray and roi
-image_process = image_gray
-image_process[roi_start:h, int(w - 0.6 * w) :] = opening_roi
+    largest_contour = max(contours, key=contour_area)
+    x, y, w, h = cv2.boundingRect(largest_contour)
 
-# Find contours and draw rectangle
-contours, _ = cv2.findContours(
-    image_process, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-)
-for cnt in contours:
-    if cv2.contourArea(cnt) > 200:
-        x, y, w, h = cv2.boundingRect(cnt)
-        cv2.rectangle(image_2, (x, y), (x + w, y + h), (0, 255, 0), 2)
-# Save image
-cv2.imwrite(f"./output_2.png", image_2)
+    noisy_region = image[y : y + h, x : x + w]
+
+    return noisy_region, (x, y, w, h)
+
+
+def preprocess_image(image_path):
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Apply adaptive threshold
+    thresh_binary = cv2.adaptiveThreshold(
+        gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 61, 2
+    )
+    kernel_lines = cv2.getStructuringElement(cv2.MORPH_RECT, (55, 1))
+    # Create an image with only the lines
+    img_lines = cv2.morphologyEx(thresh_binary, cv2.MORPH_OPEN, kernel_lines)
+    # Subtract the lines from the original image
+    process_img = cv2.subtract(thresh_binary, img_lines)
+
+    return process_img
+
+
+def remove_small_contours(process_img):
+    # Find contours
+    contours, _ = cv2.findContours(process_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        # Remove small contours have an area less than 100
+        if cv2.contourArea(contour) < 100:
+            cv2.drawContours(process_img, [contour], 0, 0, cv2.FILLED)
+
+    return process_img
+
+
+def erode_and_open(process_img):
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (2, 2))
+    process_img = cv2.erode(process_img, kernel, iterations=2)
+    process_img = cv2.morphologyEx(process_img, cv2.MORPH_OPEN, kernel, iterations=1)
+
+    return process_img
+
+
+def insert_roi(process_img, roi, x, y, h, w):
+    # Preprocess the ROI image and insert it into the processed image
+    roi = cv2.bitwise_not(roi)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 4))
+    roi = cv2.morphologyEx(roi, cv2.MORPH_OPEN, kernel, iterations=1)
+
+    process_img[y : y + h, x : x + w] = cv2.resize(roi, (w, h))
+
+    return process_img
+
+
+def draw_bounding_boxes(image, process_img):
+    # Find contours and draw bounding boxes
+    contours = cv2.findContours(
+        process_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )[0]
+    for cnt in contours:
+        if cv2.contourArea(cnt) > 150:
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(image, (x, y - 2), (x + w, y + h), (0, 255, 0), 2)
+
+    return image
+
+
+image_path = "./input2.png"
+process_img = preprocess_image(image_path)
+process_img = remove_small_contours(process_img)
+process_img = erode_and_open(process_img)
+
+roi, (x, y, h, w) = find_ROI(~process_img)
+process_img = insert_roi(process_img, roi, x, y, h, w)
+
+image = cv2.imread(image_path)
+image = draw_bounding_boxes(image, process_img)
+
+cv2.imwrite("./output_2.png", image)
